b9e6f9a206b7c255cda3f9ea7a07f2ed34ead158
Linda <linda@alyssas-air-2.home>
2020-06-16 14:39:28 -0400

    POST: create new user

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,29 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+router.post('/', async (req, res, next) => {
+  try {
+    const createUser = await User.create(req.body)
+    if (createUser) {
+      res.status(200).json(createUser)
+    } else {
+      const error = new Error('Failed to POST /api/user')
+      error.status = 500
+      throw error
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.param.userId)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
