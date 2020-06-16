1cc3c0935ffaf059d39f6995133dd4180098c43b
Dewin <dewinspena@gmail.com>
2020-06-16 14:21:00 -0400

    Added more navbar menu options, Added metrial ui to make it look better

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,30 +3,104 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+//material ui imports
+import {makeStyles} from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import AddCircleIcon from '@material-ui/icons/AddCircle'
+import ProductsIcon from '@material-ui/icons/Dehaze'
+import AccountIcon from '@material-ui/icons/ListAlt'
+import LogoffIcon from '@material-ui/icons/PowerSettingsNew'
+import Badge from '@material-ui/core/Badge'
+import IconButton from '@material-ui/core/IconButton'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>BOILERMAKER</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+const useStyles = makeStyles(theme => ({
+  navbar: {
+    padding: theme.spacing(1),
+    justifyContent: 'space-around'
+  },
+  button_menu: {
+    marginRight: '25px'
+  }
+}))
+
+const Navbar = ({handleClick, isLoggedIn}) => {
+  const classes = useStyles()
+  //in cart should be the numbers of products in the current session
+  let inCart = 2
+
+  return (
+    <div>
+      <nav>
+        <Grid container className={classes.navbar}>
+          <Grid item>
+            <h4>BOILERMAKER</h4>
+          </Grid>
+          <Grid item>
+            <Link to="/products">
+              <Button
+                className={classes.button_menu}
+                startIcon={<ProductsIcon />}
+              >
+                {' '}
+                PRODUCTS{' '}
+              </Button>
+            </Link>
+            {isLoggedIn ? (
+              <>
+                <Button
+                  className={classes.button_menu}
+                  startIcon={<AccountIcon />}
+                >
+                  {' '}
+                  MY ACCOUNT{' '}
+                </Button>
+                <Button
+                  className={classes.button_menu}
+                  color="secondary"
+                  startIcon={<LogoffIcon />}
+                  onClick={handleClick}
+                >
+                  {' '}
+                  LOGOUT{' '}
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/signup">
+                  <Button
+                    className={classes.button_menu}
+                    startIcon={<AddCircleIcon />}
+                  >
+                    {' '}
+                    REGISTER{' '}
+                  </Button>
+                </Link>
+                <Link to="/login">
+                  <Button
+                    className={classes.button_menu}
+                    startIcon={<AccountCircleIcon />}
+                  >
+                    {' '}
+                    lOGIN{' '}
+                  </Button>
+                </Link>
+              </>
+            )}
+            <IconButton aria-label="cart">
+              <Badge badgeContent={inCart} color="secondary">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+          </Grid>
+        </Grid>
+      </nav>
+      <hr />
+    </div>
+  )
+}
 
 /**
  * CONTAINER
