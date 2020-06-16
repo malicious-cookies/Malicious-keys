4604867b231d50bd873344d62be3adb3c4669ec9
Linda <linda@alyssas-air-2.home>
2020-06-16 14:23:44 -0400

    GET: get one user route created

--- server/api/users.js
+++ server/api/users.js
@@ -2,6 +2,14 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.param.userId)
+  } catch (error) {
+    next(error)
+  }
+})
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
