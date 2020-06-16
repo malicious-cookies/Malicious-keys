652c592ad24b001790e21243aabaee59613c69ab
SERGE <nsergearistide@gmail.com>
2020-06-16 14:57:21 -0400

    all keyboards component created

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
+if (process.env.NODE_ENV !== 'production') require('../../secrets')
+console.log(process.env.DB_CONNECTION)
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
