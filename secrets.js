d5f735f0a6042c0ede4feb44418e42f04f94b644
Linda Eng <90lineng@gmail.com>
2020-06-20 23:13:37 -0400

    google login complete

---
+++ secrets.js
@@ -0,0 +1,7 @@
+process.env.GOOGLE_CLIENT_ID =
+  '869618470443-07820u03kdueudt8o50khemkf2t2p3ia.apps.googleusercontent.com'
+
+process.env.GOOGLE_CLIENT_SECRET = 'jSP37b71cqmTogDE1-Wx99vO'
+
+process.env.GOOGLE_CALLBACK =
+  'https://malicious-keys.herokuapp.com/auth/google/callback'
